7c895ad3f710de4adc7c8a4a35ddbf0746560b63
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-04-29 03:09:59 +0430

    Establish backend routes for the App.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const contacts = require("./routes/contacts");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+// Define Routes
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+app.use("/api/contacts", contacts);
 
 app.get("/", (req, res) =>
   res.json({
