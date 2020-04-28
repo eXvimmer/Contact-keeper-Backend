3c23c1e2fdc9b6dc621a26a5bb50a648e5f1616f
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-04-29 03:28:50 +0430

    Connect MongoDB atlas to the app.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const contacts = require("./routes/contacts");
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
