eb7a51c0133efc3c9f48e4802dabf50b633d7662
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-04-29 06:16:07 +0430

    Create a jwt and corresponding jwtSecret and use it to response the @route POST /api/users with the user.id in form of a token.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator");
 
 // User Model
@@ -37,10 +39,31 @@ router.post(
       }
 
       user = new User({ name, email, password });
+
       const salt = await bcrypt.genSalt(10);
       user.password = await bcrypt.hash(password, salt);
+
       await user.save();
-      res.send("User created.");
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+          });
+        }
+      );
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Server Error");
